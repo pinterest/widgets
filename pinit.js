f5281a29493a6f2eedfa4444793e0f9b89ce0ea3
Kent Brewster <kentbrew@users.noreply.github.com>
2020-09-02 09:52:48 -0700

    Update pinit.js
    Always load the https version, with a cache buster.

--- pinit.js
+++ pinit.js
@@ -1,30 +1,33 @@
 /* jshint indent: false, maxlen: false */
-// Split into loader and main body; run main only if it's not been run before
 
-(function (w, d, scriptUrl) {
+(function (w, d, a) {
 
-  var firstScript, newScript, hazPinIt;
-  
-  // generate an unique-ish global ID: hazPinIt_ plus today's Unix day
+  var hazPinIt, firstScript, newScript, scriptUrl;
+
+  // generate an unique-ish global variable: hazPinIt_ plus today's Unix day
   hazPinIt = 'PIN_' + ~~(new Date().getTime() / 86400000);
-  
+
   if (!w[hazPinIt]) {
 
-    // don't run next time
-    w[hazPinIt] = true;    
-    
+    // keep track of the number of times pinit.js is included for debugging purposes
+    w[hazPinIt] = 1;
+
     // avoid KB927917 error in IE8
     w.setTimeout(function () {
-    
-      // load the bulk of pinit.js
+
+      // load payload
       firstScript = d.getElementsByTagName('SCRIPT')[0];
       newScript = d.createElement('SCRIPT');
       newScript.type = 'text/javascript';
       newScript.async = true;
-      newScript.src = scriptUrl;
+      newScript.src = a.mainUrl + '?' + Math.random();
       firstScript.parentNode.insertBefore(newScript, firstScript);
 
     }, 10);
 
+  } else {
+    w[hazPinIt] += 1;
   }
-}(window, document, '//assets.pinterest.com/js/pinit_main.js'));
+}(window, document, {
+  'mainUrl': 'https://assets.pinterest.com/js/pinit_main.js'
+}));
